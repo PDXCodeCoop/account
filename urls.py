8e17f80b3a08ad2f247a566ad918a659faa8c9d8
Braden Jageman <neurobomber@gmail.com>
2016-03-12 15:05:27 -0800

    first commit

---
+++ urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+
+from . import views
+
+urlpatterns = [
+               url(r'^$', views.profile_private, name='account'),
+               url(r'^register$', views.register, name='register'),
+               url(r'^logout$', views.logout_view, name='logout'),
+               url(r'^login$', views.login_view, name='login'),
+               url(r'^social$', views.login_social, name='login_social'),
+               url(r'^profile$', views.profile_private, name='profile'),
+               ]
